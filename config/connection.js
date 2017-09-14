9d22b33bb4855dbd4b96a53725fa32495aff9e46
Ken Thompson <ken.r.thompson@icloud.com>
2017-09-14 16:33:39 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "Dtmao#423",
+    password: "XXXXXXXX",
     database: "burgers_db"
   });
 };
